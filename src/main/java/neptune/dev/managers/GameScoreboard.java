1f420e92b0b7da8ca05f4809bb5e3ffd71014504
lrxxh <nodytamy2008@gmail.com>
2023-07-28 19:43:06 +0200

    Cleaned Code and fixed a miss type

--- src/main/java/neptune/dev/managers/GameScoreboard.java
+++ src/main/java/neptune/dev/managers/GameScoreboard.java
@@ -1,16 +1,15 @@
 package neptune.dev.managers;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import neptune.dev.Neptune;
 import neptune.dev.player.PlayerState;
 import neptune.dev.utils.PlayerUtils;
 import neptune.dev.utils.assemble.AssembleAdapter;
-import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class GameScoreboard implements AssembleAdapter {
 
@@ -21,52 +20,65 @@ public class GameScoreboard implements AssembleAdapter {
 
     @Override
     public List<String> getLines(Player player) {
-        final List<String> toReturn = new ArrayList<>();
-        if (PlayerUtils.getState(player) == PlayerState.LOBBY) {
-            return getLobby(player);
-        }
-        if (PlayerUtils.getState(player) == PlayerState.PLAYING) {
-            for (String sb : Neptune.scoreboardConfig.getStringList("scoreboard.match")) {
-                String msg = sb;
-                String player1 = MatchManager.getMatchPlayers(MatchManager.getMatchID(player)).get(0).getName();
-                String player2 = MatchManager.getMatchPlayers(MatchManager.getMatchID(player)).get(1).getName();
-                Player user;
-                Player opponent;
-                if(Bukkit.getPlayer(player1) == player){
-                    user = Bukkit.getPlayer(player1);
-                    opponent = Bukkit.getPlayer(player2);
-                }
-                else{
-                    user = Bukkit.getPlayer(player2);
-                    opponent = Bukkit.getPlayer(player1);
-                }
-                msg = msg.replace("{online_players}", String.valueOf(Neptune.instance.getServer().getOnlinePlayers().size()));
-                msg = msg.replace("{playing_players}", String.valueOf(QueueProcessor.playing));
-                msg = msg.replace("{user_ping}", String.valueOf(getPing(user)));
-                msg = msg.replace("{opponent_ping}", String.valueOf(getPing(opponent)));
-                msg = msg.replace("{opponent}", String.valueOf(opponent.getName()));
+        PlayerState playerState = PlayerUtils.getState(player);
 
-                toReturn.add(msg);
-            }
+        if (playerState == PlayerState.LOBBY) {
+            return getLobbyLines();
+        } else if (playerState == PlayerState.PLAYING) {
+            return getMatchLines(player);
         }
-        return toReturn;
+
+        return new ArrayList<>();
     }
 
-    public List<String> getLobby(Player player) {
-        List<String> toReturn = new ArrayList<>();
-        if (PlayerUtils.getState(player) == PlayerState.LOBBY) {
-            for (String sb : Neptune.scoreboardConfig.getStringList("scoreboard.lobby")) {
-                String msg = sb;
-                msg = msg.replace("{online_players}", String.valueOf(Neptune.instance.getServer().getOnlinePlayers().size()));
-                msg = msg.replace("{playing_players}", String.valueOf(QueueProcessor.playing));
-                toReturn.add(msg);
-            }
+    private List<String> getLobbyLines() {
+        List<String> lobbyLines = new ArrayList<>();
+        List<String> scoreboardLobby = Neptune.scoreboardConfig.getStringList("scoreboard.lobby");
+
+        int onlinePlayers = Neptune.instance.getServer().getOnlinePlayers().size();
+        int playingPlayers = QueueProcessor.playing;
+
+        for (String line : scoreboardLobby) {
+            line = line.replace("{online_players}", String.valueOf(onlinePlayers));
+            line = line.replace("{playing_players}", String.valueOf(playingPlayers));
+            lobbyLines.add(line);
         }
-        return toReturn;
+        return lobbyLines;
     }
 
+    private List<String> getMatchLines(Player player) {
+        List<String> matchLines = new ArrayList<>();
+        List<String> scoreboardMatch = Neptune.scoreboardConfig.getStringList("scoreboard.match");
+        UUID matchID = MatchManager.getMatchID(player);
+        List<Player> matchPlayers = MatchManager.getMatchPlayers(matchID);
 
-    public int getPing(Player player) {
+        Player user = matchPlayers.get(0);
+        Player opponent = matchPlayers.get(1);
+
+        if (opponent == player) {
+            // Swap players to ensure the user is always the current player
+            Player temp = user;
+            user = opponent;
+            opponent = temp;
+        }
+
+        int onlinePlayers = Neptune.instance.getServer().getOnlinePlayers().size();
+        int playingPlayers = QueueProcessor.playing;
+        int userPing = getPing(user);
+        int opponentPing = getPing(opponent);
+
+        for (String line : scoreboardMatch) {
+            line = line.replace("{online_players}", String.valueOf(onlinePlayers));
+            line = line.replace("{playing_players}", String.valueOf(playingPlayers));
+            line = line.replace("{user_ping}", String.valueOf(userPing));
+            line = line.replace("{opponent_ping}", String.valueOf(opponentPing));
+            line = line.replace("{opponent}", opponent.getName());
+            matchLines.add(line);
+        }
+        return matchLines;
+    }
+
+    private int getPing(Player player) {
         return ((CraftPlayer) player).getHandle().ping;
     }
 }
