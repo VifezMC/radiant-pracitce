f69a74e5a040b79d261f9fd649ef4343460df16d
lrxxh <nodytamy2008@gmail.com>
2023-07-29 16:21:46 +0200

    Removed useless code

--- src/main/java/neptune/dev/game/Arena.java
+++ src/main/java/neptune/dev/game/Arena.java
@@ -18,23 +18,12 @@ public class Arena {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Location getSpawn1() {
         return spawn1;
-    }
-
-    public void setSpawn1(Location spawn1) {
-        this.spawn1 = spawn1;
     }
 
     public Location getSpawn2() {
         return spawn2;
     }
 
-    public void setSpawn2(Location spawn2) {
-        this.spawn2 = spawn2;
-    }
 }
