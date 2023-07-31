48b892ecc080e52ca2baf991dbf4fd8a8ec8520f
lrxxh <nodytamy2008@gmail.com>
2023-07-31 10:57:05 +0200

    Removed old info

--- src/main/java/neptune/dev/Constants.java
+++ src/main/java/neptune/dev/Constants.java
@@ -2,8 +2,8 @@ package neptune.dev;
 
 public class Constants {
     public static final String PlName = "Netptune";
-    public static final String Autor = "Ocean Development";
+    public static final String Autor = "lrxh";
     public static final double Ver = 1.0;
-    public static final String Discord = "discord.oceandev.net";
+    public static final String Discord = "discord.";
 
 }
