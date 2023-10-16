95dc9d301d882e56137e2500e2e3edbb09ac7f2a
lrxxh <nodytamy2008@gmail.com>
2023-10-16 17:44:12 +0200

    Added placeholder for division

--- src/main/java/neptune/dev/Placeholder.java
+++ src/main/java/neptune/dev/Placeholder.java
@@ -2,6 +2,7 @@ package neptune.dev;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import neptune.dev.listeners.PlayerDataListener;
+import neptune.dev.managers.ConfigManager;
 import neptune.dev.types.Stat;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -40,14 +41,22 @@ public class Placeholder extends PlaceholderExpansion {
             return "";
         }
 
-        if (identifier.equals("elo")) {
-            return getPlayerELO(player) + "";
+        switch (identifier) {
+            case "elo":
+                return getPlayerELO(player) + "";
+            case "division":
+                return getPlayerDivision(player);
+            default:
+                return null;
         }
-        return null;
     }
 
     private int getPlayerELO(OfflinePlayer p) {
         Stat stat = PlayerDataListener.getStats(p);
         return stat.getELO();
+    }
+    private String getPlayerDivision(OfflinePlayer p) {
+        Stat stat = PlayerDataListener.getStats(p);
+        return ConfigManager.divisionsManager.getPlayerDivision(stat.getELO());
     }
 }
