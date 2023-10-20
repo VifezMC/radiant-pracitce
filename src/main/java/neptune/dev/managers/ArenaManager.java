963092dafbcd03c5a51e6542b715b895629f94a1
lrxxh <nodytamy2008@gmail.com>
2023-10-20 21:22:10 +0200

    Made it so dropped items in arena will get removed

--- src/main/java/neptune/dev/managers/ArenaManager.java
+++ src/main/java/neptune/dev/managers/ArenaManager.java
@@ -7,6 +7,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -47,33 +49,49 @@ public class ArenaManager {
     }
 
     public static void resetArena(Location lowEdge, Location highEdge, Player p, String kitName) {
-        if(KitManager.getKit(kitName).getRules().contains("build")){
+        if (KitManager.getKit(kitName).getRules().contains("build")) {
 
+            World world = lowEdge.getWorld();
 
+            int minX = Math.min(lowEdge.getBlockX(), highEdge.getBlockX());
+            int minY = Math.min(lowEdge.getBlockY(), highEdge.getBlockY());
+            int minZ = Math.min(lowEdge.getBlockZ(), highEdge.getBlockZ());
 
-        World world = lowEdge.getWorld();
+            int maxX = Math.max(lowEdge.getBlockX(), highEdge.getBlockX());
+            int maxY = Math.max(lowEdge.getBlockY(), highEdge.getBlockY());
+            int maxZ = Math.max(lowEdge.getBlockZ(), highEdge.getBlockZ());
 
-        int minX = Math.min(lowEdge.getBlockX(), highEdge.getBlockX());
-        int minY = Math.min(lowEdge.getBlockY(), highEdge.getBlockY());
-        int minZ = Math.min(lowEdge.getBlockZ(), highEdge.getBlockZ());
-
-        int maxX = Math.max(lowEdge.getBlockX(), highEdge.getBlockX());
-        int maxY = Math.max(lowEdge.getBlockY(), highEdge.getBlockY());
-        int maxZ = Math.max(lowEdge.getBlockZ(), highEdge.getBlockZ());
-
-        for (int x = minX; x <= maxX; x++) {
-            for (int y = minY; y <= maxY; y++) {
-                for (int z = minZ; z <= maxZ; z++) {
-                    Block block = world.getBlockAt(x, y, z);
-                    if (block.getType() == Material.WATER || block.getType() == Material.LAVA || block.getType() == Material.STATIONARY_WATER || block.getType() == Material.STATIONARY_LAVA) {
-                        block.setType(Material.AIR);
+            for (int x = minX; x <= maxX; x++) {
+                for (int y = minY; y <= maxY; y++) {
+                    for (int z = minZ; z <= maxZ; z++) {
+                        Block block = world.getBlockAt(x, y, z);
+                        if (block.getType() == Material.WATER || block.getType() == Material.LAVA || block.getType() == Material.STATIONARY_WATER || block.getType() == Material.STATIONARY_LAVA) {
+                            block.setType(Material.AIR);
+                        }
                     }
                 }
             }
+            for (Entity entity : world.getEntities()) {
+                if (entity instanceof Item) {
+                    Location itemLocation = entity.getLocation();
+                    if (isLocationWithinArena(itemLocation, lowEdge, highEdge)) {
+                        entity.remove();
+                    }
+                }
             }
-        BlockListener.removeBlocks(p);
+
+            BlockListener.removeBlocks(p);
         }
     }
+    private static boolean isLocationWithinArena(Location location, Location lowEdge, Location highEdge) {
+        return location.getBlockX() >= lowEdge.getBlockX() &&
+                location.getBlockX() <= highEdge.getBlockX() &&
+                location.getBlockY() >= lowEdge.getBlockY() &&
+                location.getBlockY() <= highEdge.getBlockY() &&
+                location.getBlockZ() >= lowEdge.getBlockZ() &&
+                location.getBlockZ() <= highEdge.getBlockZ();
+    }
+
 
     public static Arena selectRandomAvailableArena(List<String> arenas) {
         List<Arena> availableArenas = new ArrayList<>();
