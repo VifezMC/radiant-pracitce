07223de5bdcb46a1ad55eac102c93e7f97ff1eb5
VifezMC <VifezMC@gmail.com>
2023-11-03 23:18:24 +0000

    Changed Kit & Arena Format

--- src/main/java/xyz/kiradev/commands/admin/ArenaCMD.java
+++ src/main/java/xyz/kiradev/commands/admin/ArenaCMD.java
@@ -102,18 +102,19 @@ public class ArenaCMD implements CommandExecutor {
 
     private void showArenaCommands(Player player) {
         player.sendMessage(CC.translate(""));
-        player.sendMessage(CC.translate("-------------------------------------------------------------"));
+        player.sendMessage(CC.translate("==============================================================="));
         player.sendMessage(CC.translate("&d&lStellar Arena Setup:"));
         player.sendMessage(CC.translate("&d/arena create &8<&7name&8> &7- &8(&7Create an arena&8)"));
         player.sendMessage(CC.translate("&d/arena remove &8<&7name&8> &7- &8(&7Remove an arena&8)"));
         player.sendMessage(CC.translate("&d/arena save &8<&7name&8> &7- &8(&7Save arenas&8)"));
         player.sendMessage(CC.translate(" "));
-        player.sendMessage(CC.translate("-------------------------------------------------------------"));
+        player.sendMessage(CC.translate("==============================================================="));
         player.sendMessage(CC.translate("&d&lStellar Arena Spawn commands:"));
         player.sendMessage(CC.translate("&d/arena a &8<&7name&8> &7- &8(&7Set the first player spawn&8)"));
         player.sendMessage(CC.translate("&d/arena b &8<&7name&8> &7- &8(&7Set the second player spawn&8)"));
         player.sendMessage(CC.translate("&d/arena min &8<&7name&8> &7- &8(&7Set the first player spawn&8)"));
         player.sendMessage(CC.translate("&d/arena max &8<&7name&8> &7- &8(&7Set the second player spawn&8)"));
+        player.sendMessage(CC.translate("==============================================================="));
         player.sendMessage(CC.translate(""));
     }
 }
