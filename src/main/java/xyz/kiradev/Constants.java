0bb8c4032a896fc8fdb8a6749f879d6f9b895b44
Vifez <122062858+VifezMC@users.noreply.github.com>
2023-11-19 20:04:37 +0000

    Changed constants

--- src/main/java/xyz/kiradev/Constants.java
+++ src/main/java/xyz/kiradev/Constants.java
@@ -3,7 +3,7 @@ package xyz.kiradev;
 public class Constants {
     public static final String PlName = "Stellar";
     public static final String Author = "Vifez, lrxh";
-    public static final double Ver = 1.1;
+    public static final double Ver = 1.4;
     public static final String Discord = "https://discord.gg/QmqsCy5xtu";
 
 }
